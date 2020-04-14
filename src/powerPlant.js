8a31c465c32d0cbb4646f8fdc56c2a80e09f7037
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-14 11:25:20 -0700

    Refactor to a more modular function with state prop and value
    Co-Authored-By: Steph Podolak <StephPodolak@gmail.com>
    Co-Authored-By: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-Authored-By: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/powerPlant.js
+++ src/powerPlant.js
@@ -12,24 +12,33 @@
 //   }
 // };
 
-const changePlantState = (plant, property) => {
-  return {
-    ...plant,
-    [property]: (plant[property] || 0) + 1
-  }
-}
+// const changePlantState = (plant, property) => {
+//   return {
+//     ...plant,
+//     [property]: (plant[property] || 0) + 1
+//   }
+// }
 
-let plant = { soil: 0, light: 0, water: 0 };
-const newPlant = changePlantState(plant, "soil");
-
-console.log(newPlant);
-
-
+const changeState = (state, prop, value) => ({
+    ...state,
+    [prop]: (state[prop] || 0) + value
+})
 
 
+
+
+
+// (parameters) => <return statement>
+/////\\\\\\\\\//////\\\\\\///\\\
+///////////////\\\\\\\\\\\\\\\\\\
+// ----- Great Notes ------ /////\
+///////////////\\\\\\\\\\\\\\\\\\\
+// let plant = { soil: 0, light: 0, water: 0 };
+// const newPlant = changePlantState(plant, "soil");
+
+// console.log(newPlant);
 
 // In theory, the function isn't mutating state because we are returning a new object. (Unfortunately, in reality, JavaScript is still referencing - and mutating - the original object because it creates a shallow clone, not a deep clone. But we'd need to use a special library to ensure we have a deep clone, so we won't worry about that right now!)
-
 
 // const plant = {};
 
@@ -46,3 +55,35 @@ console.log(newPlant);
 //   soil: 0,
 //   light: 0
 // }
+
+/*
+                    ____
+                 _.' :  `._
+             .-.'`.  ;   .'`.-.
+    __      / : ___\ ;  /___ ; \      __
+  ,'_ ""--.:__;".-.";: :".-.":__;.--"" _`,
+  :' `.t""--.. '<@.`;_  ',@>` ..--""j.' `;
+       `:-.._J '-.-'L__ `-- ' L_..-;'
+         "-.__ ;  .-"  "-.  : __.-"
+             L ' /.------.\ ' J
+              "-.   "--"   .-"
+             __.l"-:_JL_;-";.__
+          .-j/'.;  ;""""  / .'\"-.
+        .' /:`. "-.:     .-" .';  `.
+     .-"  / ;  "-. "-..-" .-"  :    "-.
+  .+"-.  : :      "-.__.-"      ;-._   \
+  ; \  `.; ;                    : : "+. ;
+  :  ;   ; ;                    : ;  : \:
+ : `."-; ;  ;                  :  ;   ,/;
+  ;    -: ;  :                ;  : .-"'  :
+  :\     \  : ;             : \.-"      :
+   ;`.    \  ; :            ;.'_..--  / ;
+   :  "-.  "-:  ;          :/."      .'  :
+     \       .-`.\        /t-""  ":-+.   :
+      `.  .-"    `l    __/ /`. :  ; ; \  ;
+        \   .-" .-"-.-"  .' .'j \  /   ;/
+         \ / .-"   /.     .'.' ;_:'    ;
+          :-""-.`./-.'     /    `.___.'
+                \ `t  ._  /  bug :F_P:
+                 "-.t-._:'
+*/
