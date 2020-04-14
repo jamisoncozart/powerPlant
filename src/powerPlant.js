62df2f8c48bc5029834df04787ec9289b08df590
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-14 11:10:09 -0700

    Refactor changing plant state into one function
    Co-Authored-By: Steph Podolak <StephPodolak@gmail.com>
    Co-Authored-By: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-Authored-By: Dominique Youmans <46074403+21-Coding@users.noreply.github.com>

--- src/powerPlant.js
+++ src/powerPlant.js
@@ -1,19 +1,45 @@
-const hydrate = (plant) => {
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// }
+
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
+
+const changePlantState = (plant, property) => {
   return {
     ...plant,
-    water: (plant.water || 0) + 1
+    [property]: (plant[property] || 0) + 1
   }
 }
 
-const plant = {};
+let plant = { soil: 0, light: 0, water: 0 };
+const newPlant = changePlantState(plant, "soil");
 
-console.log(plant.water);
+console.log(newPlant);
 
-const plantWithWater = hydrate(plant);
-const reallyWateredPlant = hydrate(plantWithWater);
 
-console.log(plantWithWater.water);
-console.log(reallyWateredPlant.water);
+
+
+
+// In theory, the function isn't mutating state because we are returning a new object. (Unfortunately, in reality, JavaScript is still referencing - and mutating - the original object because it creates a shallow clone, not a deep clone. But we'd need to use a special library to ensure we have a deep clone, so we won't worry about that right now!)
+
+
+// const plant = {};
+
+// console.log(plant.water);
+
+// const plantWithWater = hydrate(plant);
+// const reallyWateredPlant = hydrate(plantWithWater);
+
+// console.log(plantWithWater.water);
+// console.log(reallyWateredPlant.water);
 
 // {
 //   water: 0,
